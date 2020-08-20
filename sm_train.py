1ba23e45a1948d7793fd5c60eb4b56133dee8386
JunYeopLee <junyeoplee91@gmail.com>
2020-08-20 06:22:55 +0000

    to use managed spot instance

--- sm_train.py
+++ sm_train.py
@@ -28,6 +28,7 @@ estimator = PyTorch(entry_point='mnist.py',
                     py_version='py3',
                     instance_count=2,
                     instance_type='ml.c4.xlarge',
+                    use_spot_instances=True,
                     metric_definitions=metric_definitions,
                     hyperparameters={
                         'epochs': 1,
